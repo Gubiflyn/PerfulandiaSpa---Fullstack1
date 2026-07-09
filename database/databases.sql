7b43271200254e038c6c9a4337e922fdccbcc6d3
Gubier Zamora <gub.zamora@duocuc.cl>
2026-07-09 03:53:03 -0400

    agrega base de datos MySQL con Docker

---
+++ database/databases.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS perfulandia_catalogo
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS perfulandia_usuarios
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS perfulandia_ventas
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS perfulandia_contacto
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
